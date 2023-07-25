0a4d537d18e7644e085e41bf9e5232dde3c9f652
Krishna Rathod <krishna.rathod227@gmail.com>
2023-07-25 14:31:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+import Login from './components/Authentication/Login'
+import Register from './components/Authentication/Register'
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
+import Cart from './Pages/Cart/Cart';
+// import Product from './components/Products/Product';
+import Home from './Pages/Home';
+import ProductDetails from './components/ProductDetail/ProductDetails';
+import Address from './components/Address/Address';
+import Checkout from './components/Checkout/Checkout';
+import Footer from './components/Footer/Footer';
+import NavProduct from './components/Products/Nav_product';
+import SearchProduct from './components/Products/SearchProduct';
 
 function App() {
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart/address" element={<Address />} />
+          <Route path="/cart/address/checkout" element={<Checkout />} />
+          <Route path='/product/category' element={<NavProduct />} />
+          <Route path='/search' element={<SearchProduct />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
